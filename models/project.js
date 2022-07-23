794c5acd1ae776d0f5e3d8e9363091da5033ccd5
Ikhena Owen <ikhena.owen47@gmail.com>
2022-07-23 14:32:21 +0100

    fix import bug

--- models/project.js
+++ models/project.js
@@ -1,16 +1,14 @@
-const {Schema} = require("mongoose")
+const { Schema, model } = require("mongoose");
 const ProjectSchema = new Schema({
-  const {model} = require('mongoose')
   name: {
     type: String,
-    required: true
+    required: true,
   },
   issues: [
     {
-      type: Schema.Types.ObjectId, 
-      ref: "Issue"
-    }
-  ]
-
-})
-module.exports = model('Project', ProjectSchema) 
+      type: Schema.Types.ObjectId,
+      ref: "Issue",
+    },
+  ],
+});
+module.exports = model("Project", ProjectSchema);
