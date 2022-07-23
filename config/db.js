0bb6531ab7d944870f1bb1051581706904f9f2fa
Ikhena Owen <ikhena.owen47@gmail.com>
2022-07-23 16:04:43 +0100

    log error correctly

--- config/db.js
+++ config/db.js
@@ -1,4 +1,7 @@
 const mongoose = require("mongoose");
-mongoose.connect(process.env.MONGO_URI).then(() => {
-  console.log("connected to db");
-}).catch(err => console.log(error))
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("connected to db");
+  })
+  .catch((err) => console.log(err));
