805b4b9fbd31edfc99016ec7db10588aa1533320
Ikhena Owen <ikhena.owen47@gmail.com>
2022-07-23 10:06:42 +0100

    create issue model

---
+++ models/issue.js
@@ -0,0 +1,34 @@
+const { model, Schema } = require("mongoose");
+const IssueSchema = new Schema({
+  issue_title: {
+    type: String,
+    required: true,
+  },
+  issue_text: {
+    type: String,
+    required: true,
+  },
+  created_on: {
+    type: Date,
+    default: new Date(),
+  },
+  updated_on: {
+    type: Date,
+    default: new Date(),
+  },
+  created_by: {
+    type: String,
+    required: true,
+  },
+  assigned_to: String,
+  open: {
+    type: Boolean,
+    default: true,
+  },
+  status_text: String,
+});
+project: {
+  type: Schema.Types.ObjectId,
+    ref: "Project"
+}
+module.exports = model("Issue", IssueSchema);
